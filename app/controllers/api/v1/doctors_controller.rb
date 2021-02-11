8b4e8bf59031e54c65fda08a75eab432d34650b6
wickedvic <viksipart@gmail.com>
2021-02-10 22:05:42 -0500

    auth bug fixed for doc's pats

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::DoctorsController < ApplicationController
+    before_action :authorized, only: [:profile]
 
     def profile
         render json: { user: current_user }, status: :accepted
     end
 
     def show
-        @doctor = Doctor.find(params[:id])
-        render json: @doctor, include: :users
+        doctor = Doctor.find(params[:id])
+        render json: doctor, include: :users
     end
 end 
